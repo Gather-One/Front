e3923b5fbd05e936cdbb3bd29248067fa33a74ad
JunYoungKr <kop981020@naver.com>
2024-11-26 23:23:40 +0900

    프로젝트 초기화

--- app/types/usePostMessage.ts
+++
@@ -1,4 +0,0 @@
-export interface WebViewMessage {
-  type: string; // 메시지 타입 (예: "event" or "action")
-  payload?: any; // 전달하려는 데이터
-}
--- web/src/types/types.ts
+++
@@ -1,4 +0,0 @@
-export interface WebViewMessage {
-  type: string; // 메시지 타입 (예: "event", "action", "response" 등)
-  payload?: string; // 추가 데이터
-}
