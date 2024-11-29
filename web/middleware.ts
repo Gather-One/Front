db9d24362475633ccf17949b66258450d4fe9c8a
JunYoungKr <kop981020@naver.com>
2024-11-29 19:06:36 +0900

    App 프로젝트 삭제

---
+++ web/middleware.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  return res;
+}
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import React from "react";
 import { useEffect } from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import checkAnimation from "../../public/checkAnimation.json";
 
 const Home = () => {
   useEffect(() => {
@@ -17,13 +20,22 @@ const Home = () => {
   }, []);
 
   const sendMessageToReactNative = () => {
+    console.log("Send Message to React");
     // React Native로 메시지 전송
-    (window as any).ReactNativeWebView?.postMessage("Hello from Web!");
+    window.ReactNativeWebView?.postMessage("Hello from Web!");
   };
 
   return (
     <div>
       <h1>Next.js WebView 통신 테스트</h1>
+      <div style={{ backgroundColor: "white" }}>
+        <Player
+          autoplay
+          loop
+          src={checkAnimation}
+          style={{ height: "150px", width: "150px" }}
+        />
+      </div>
       <button onClick={sendMessageToReactNative}>
         Send Message to React Native
       </button>
